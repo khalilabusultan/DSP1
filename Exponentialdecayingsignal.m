85a857be43c3f967b31f8b9c5be9d6474bfdeca9
Dalia Tariq <daliatariq95@gmail.com>
2019-02-19 19:16:36 -0800

    remove files

--- Exponentialdecayingsignal.m
+++ Exponentialdecayingsignal.m
@@ -5,6 +5,7 @@
  N=input(' Enter Number of Samples : ');
  a=0.5
  n=0:1:N
- x=a.^ nstem(n,x);xlabel('Time');
+ x=a.^ n
+ stem(n,x);xlabel('Time');
  ylabel('Amplitude');
  title('Exponential Decaying Signal Response'); 
--- impulseResponse.m
+++ impulseResponse.m
@@ -4,7 +4,7 @@ close all;
 disp('UNIT I MPUL SE SIGNAL ');
 N=input(' E nter Number of Samples: ');
 n=-N:1:N
-x=[zeros(1,N) 1 zeros(1,N)]
+x=[zeros(1,N) 1 ones(1,N)]
 stem(n,x);
 xlabel('Time');
 ylabel('Amplitude');
